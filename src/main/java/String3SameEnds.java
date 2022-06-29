fa48810294653083fd9080d52a9b8f86e32af4bb
jeyhun <jeyhun89>
2022-06-29 07:36:49 -0400

    String3SameEnds commited

---
+++ src/main/java/String3SameEnds.java
@@ -0,0 +1,30 @@
+/**
+ * Given a string, return the longest substring that appears at both the beginning and
+ * end of the string without overlapping. For example, sameEnds("abXab") is "ab".
+ *
+ *
+ * sameEnds("abXYab") → "ab"
+ * sameEnds("xx") → "x"
+ * sameEnds("xxx") → "x"
+ */
+
+public class String3SameEnds {
+
+    public String sameEnds(String string) {
+
+        String result = "";
+
+        for(int i=0; i <= string.length()/2; i++){
+
+            String sub = string.substring(0, i);
+
+            if(string.startsWith(sub) && string.endsWith(sub)){
+
+                result = sub;
+
+            }
+        }
+        return result;
+    }
+
+}
