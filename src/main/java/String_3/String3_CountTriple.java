07176da60eaec0536d3fa0d6e0f767eb54ae715f
jeyhun <jeyhun89>
2022-06-29 07:42:01 -0400

    Created new package for String3

--- src/main/java/String_3/String3_CountTriple.java
+++ src/main/java/String_3/String3_CountTriple.java
@@ -1,3 +1,5 @@
+package String_3;
+
 public class String3_CountTriple {
 
     //We'll say that a "triple" in a string is a char appearing three times in a row. Return the number of triples in the given string. The triples may overlap.
--- src/main/java/String_3/String3_SameEnds.java
+++ src/main/java/String_3/String3_SameEnds.java
@@ -1,3 +1,5 @@
+package String_3;
+
 /**
  * Given a string, return the longest substring that appears at both the beginning and
  * end of the string without overlapping. For example, sameEnds("abXab") is "ab".
