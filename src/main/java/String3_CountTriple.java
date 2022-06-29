3f923cc3c0550fc59ed1b984c360f8754076184c
jeyhun <jeyhun89>
2022-06-29 07:38:31 -0400

    String3SameEnds commited

--- src/main/java/String3_CountTriple.java
+++ src/main/java/String3_CountTriple.java
@@ -1,4 +1,4 @@
-public class String3CountTriple {
+public class String3_CountTriple {
 
     //We'll say that a "triple" in a string is a char appearing three times in a row. Return the number of triples in the given string. The triples may overlap.
     //
